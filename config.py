a46d3822cb318b9e2bf2eef0aec9e8402087f514
krk <72791356+scrooge001@users.noreply.github.com>
2024-07-21 19:58:09 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from os import environ
 
-API_ID = int(environ.get("API_ID", "29228879"))
-API_HASH = environ.get("API_HASH", "f8c420d7f974ef0a2bc23a9a4a0dc859")
-BOT_TOKEN = environ.get("BOT_TOKEN", "7455295988:AAGMbx9vbyNiQGd69TsDitacns66xBjxK8I")
-ADMIN = int(environ.get("ADMIN", "7044258634"))          
+API_ID = int(environ.get("API_ID", ""))
+API_HASH = environ.get("API_HASH", "")
+BOT_TOKEN = environ.get("BOT_TOKEN", "")
+ADMIN = int(environ.get("ADMIN", ""))          
 CAPTION = environ.get("CAPTION", "<a href='t.me/adgmovies'>\n{file_name}\n\n┏━━━━•❅•°•❈•°•❅•━━━━┓\n💥★ ADG MOVIES™ ★💥\n┗━━━━•❅•°•❈•°•❅•━━━━┛</a>")
 
 # for thumbnail ( back end is MrMKN brain 😉)
